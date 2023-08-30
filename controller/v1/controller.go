ddbc3a6db5e9a3803db855d2aec1865784fd0627
Seunghoon-Oh <steveoh5419@gmail.com>
2023-08-30 02:54:13 +0000

    svc 업데이트

---
+++ controller/v1/controller.go
@@ -0,0 +1,14 @@
+package v1
+
+import (
+	"net/http"
+
+	"github.com/Seunghoon-Oh/cloud-ml-pipeline-manager/service"
+	"github.com/gin-gonic/gin"
+)
+
+func GetPipelines(c *gin.Context) {
+	data := service.GetPipelines()
+	println(data)
+	c.String(http.StatusOK, data)
+}
---
+++ service/pipeline.go
@@ -0,0 +1,6 @@
+package service
+
+func GetPipelines() string {
+	pipelines := "pipeline-1"
+	return pipelines
+}
